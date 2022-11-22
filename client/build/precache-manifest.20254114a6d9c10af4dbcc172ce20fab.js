77f795c8ab62fe1609b5475239f054b30b910f37
AmeyaPandit007 <69378214+AmeyaPandit007@users.noreply.github.com>
2022-11-22 14:27:53 +0530

    Add files via upload

---
+++ client/build/precache-manifest.20254114a6d9c10af4dbcc172ce20fab.js
@@ -0,0 +1,66 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "b20b1bc601d6952928ac7adf237375ab",
+    "url": "/index.html"
+  },
+  {
+    "revision": "6ef34e8a9189cd01817b",
+    "url": "/static/css/4.ac4b9a54.chunk.css"
+  },
+  {
+    "revision": "a70f04ab28dda887bb12",
+    "url": "/static/css/5.ae3fe895.chunk.css"
+  },
+  {
+    "revision": "efeaf276726fd4a6b33d",
+    "url": "/static/css/6.1118189a.chunk.css"
+  },
+  {
+    "revision": "69d489e68745d10a9984",
+    "url": "/static/css/7.f58bf3d5.chunk.css"
+  },
+  {
+    "revision": "e6fbe81d9f87d3b0d1d3",
+    "url": "/static/css/8.bdfbd312.chunk.css"
+  },
+  {
+    "revision": "8a453ea1520392352679",
+    "url": "/static/css/main.d066e63d.chunk.css"
+  },
+  {
+    "revision": "8e3ad360f84da1e3ea7d",
+    "url": "/static/js/2.2e9ce2ca.chunk.js"
+  },
+  {
+    "revision": "424039b93d0620a2c4ae",
+    "url": "/static/js/3.518742c9.chunk.js"
+  },
+  {
+    "revision": "6ef34e8a9189cd01817b",
+    "url": "/static/js/4.7f7785d6.chunk.js"
+  },
+  {
+    "revision": "a70f04ab28dda887bb12",
+    "url": "/static/js/5.c131173e.chunk.js"
+  },
+  {
+    "revision": "efeaf276726fd4a6b33d",
+    "url": "/static/js/6.76cda2a7.chunk.js"
+  },
+  {
+    "revision": "69d489e68745d10a9984",
+    "url": "/static/js/7.d713842c.chunk.js"
+  },
+  {
+    "revision": "e6fbe81d9f87d3b0d1d3",
+    "url": "/static/js/8.2050a761.chunk.js"
+  },
+  {
+    "revision": "8a453ea1520392352679",
+    "url": "/static/js/main.59b34005.chunk.js"
+  },
+  {
+    "revision": "540aa6e12dae09e652be",
+    "url": "/static/js/runtime-main.4eaf3ff4.js"
+  }
+]);
